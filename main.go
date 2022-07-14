eb908804dec3eee4ee885e3ef05f454919c853a8
Zytekaron <zytekaron@gmail.com>
2022-07-13 19:01:56 -0700

    update main to start server

--- main.go
+++ main.go
@@ -2,18 +2,32 @@ package main
 
 import (
 	"fsrv/src/config"
+	"fsrv/src/database"
+	"fsrv/src/server"
 	"log"
+	"strconv"
 )
 
 var cfg *config.Config
+var configPaths = []string{"/etc/fsrv/config.toml", "config.toml"}
+
+var keys database.KeyController
+var ratelimits database.RateLimitController
 
 func init() {
 	var err error
-	cfg, err = config.Load([]string{"config.example.toml"})
+	cfg, err = config.Load(configPaths)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
 func main() {
+	serv := server.New(keys, ratelimits)
+
+	addr := ":" + strconv.Itoa(int(cfg.Server.Port))
+	err := serv.Start(addr)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
