9e79d53844059ce014bb05633c8a504d08f00a1c
Zytekaron <zytekaron@gmail.com>
2022-07-15 21:45:42 -0700

    use permission flags for rw + todo remove AccessStatus

--- src/database/entities/permission.go
+++ src/database/entities/permission.go
@@ -1,6 +1,6 @@
 package entities
 
-type AccessStatus int8
+type AccessStatus int8 // todo: consider removing this type in favor of using presence to represent neutrality
 
 const (
 	AccessDenied AccessStatus = iota - 1
@@ -20,12 +20,25 @@ type AccessNode struct {
 	Type AccessNodeType
 }
 
+type Flags int8
+
+const (
+	// FlagPublicRead means any user can read this file without authentication.
+	FlagPublicRead Flags = 1 << iota
+	// FlagPublicWrite means any user can read this file without authentication.
+	FlagPublicWrite
+	// FlagAuthedRead means any authenticated can read this file.
+	FlagAuthedRead
+	// FlagAuthedWrite means any authenticated can read this file.
+	FlagAuthedWrite
+)
+
 // Permission represents an access specifier for a file or directory.
 type Permission struct {
 	// ID is the id of this permission
 	ID int `json:"id"`
-	// Public represents whether this permission should allow all access attempts.
-	Public bool `json:"public"`
+	// Flags represents whether this permission should allow all access attempts.
+	Flags Flags `json:"public"`
 
 	// ReadNodes represents keys and roles which may be allowed or denied access to read.
 	ReadNodes map[AccessNode]AccessStatus `json:"read_nodes"`
@@ -34,9 +47,16 @@ type Permission struct {
 }
 
 func (p *Permission) CheckRead(key *Key) AccessStatus {
-	if p.Public {
+	if key == nil {
+		if (p.Flags & FlagPublicRead) == FlagPublicRead {
+			return AccessAllowed
+		}
+		return AccessDenied
+	}
+	if (p.Flags & FlagAuthedRead) == FlagAuthedRead {
 		return AccessAllowed
 	}
+
 	if status, ok := p.ReadNodes[AccessNode{key.ID, AccessNodeKey}]; ok {
 		return status
 	}
