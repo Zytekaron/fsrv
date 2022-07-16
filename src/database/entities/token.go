95bd10c6597e1ed6de6b5945f7d974f98cc1235a
Zytekaron <zytekaron@gmail.com>
2022-07-15 21:44:35 -0700

    add fields to token type

--- src/database/entities/token.go
+++ src/database/entities/token.go
@@ -1,6 +1,27 @@
 package entities
 
+import (
+	"fsrv/utils/serde"
+	"time"
+)
+
 // Token represents an administrator API token used to authenticate for privileged actions.
 type Token struct {
+	// ID is the id of the token.
 	ID string `json:"id"`
+
+	Comment string `json:"comment"`
+
+	// ExpiresAt is the time when this token expires.
+	ExpiresAt serde.Time `json:"expires_at"`
+	// CreatedAt is the time when this token was created.
+	CreatedAt serde.Time `json:"created_at"`
+}
+
+func (t *Token) IsExpired() bool {
+	expiry := time.Time(t.ExpiresAt)
+	if expiry.UnixNano() == 0 {
+		return false
+	}
+	return time.Since(expiry).Nanoseconds() > 0
 }
