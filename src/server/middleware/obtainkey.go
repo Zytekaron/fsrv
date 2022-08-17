1ff1dd6afb3bef228fe3c65c81aa461dac7644a9
jd <johnnytriple33@gmail.com>
2022-08-16 23:46:10 -0700

    Added middleware to obtain key from KeyID

---
+++ src/server/middleware/obtainkey.go
@@ -0,0 +1,21 @@
+package middleware
+
+import (
+	"fsrv/src/database/dbutil"
+	"fsrv/src/types/response"
+	"github.com/gin-gonic/gin"
+)
+
+// ObtainKey retrieves data for a specific key and adds it to the context
+func ObtainKey(db dbutil.DBInterface) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		key, err := db.GetKeyData(ctx.GetString("KeyID"))
+		if err != nil {
+			ctx.AbortWithStatusJSON(401, response.Forbidden)
+			return
+		}
+
+		ctx.Set("key", key)
+		ctx.Next()
+	}
+}
