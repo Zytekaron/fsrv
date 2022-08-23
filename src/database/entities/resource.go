65b7f61e2c0d34c0caea4048adda119d66aa560a
Zytekaron <zytekaron@gmail.com>
2022-08-23 05:41:43 -0700

    refactor resource nodes into one field

--- src/database/entities/resource.go
+++ src/database/entities/resource.go
@@ -1,5 +1,7 @@
 package entities
 
+import "fsrv/src/types"
+
 type AccessStatus int8
 
 const (
@@ -22,61 +24,65 @@ type Resource struct {
 	// Flags represents the access flags for this resource.
 	Flags Flags `json:"flags"`
 
-	// ReadNodes represents keys and roles which may be allowed or denied access to read.
-	ReadNodes map[string]bool `json:"read_nodes"`
-	// WriteNodes represents keys and roles which may be allowed or denied access to write.
-	WriteNodes map[string]bool `json:"write_nodes"`
-	// WriteNodes represents keys and roles which may be allowed or denied access to modify.
-	ModifyNodes map[string]bool `json:"modify_nodes"`
-	// DeleteNodes represents keys and roles which may be allowed or denied access to delete.
-	DeleteNodes map[string]bool `json:"delete_nodes"`
+	// OperationNodes represents keys and roles which may be allowed or denied permission to perform operations.
+	OperationNodes map[ResourceOperationAccess]bool `json:"nodes"`
+}
+
+type ResourceOperationAccess struct {
+	// ID is the key id or role name for this node.
+	ID string
+	// Type is the operation type this node is for.
+	Type types.OperationType
 }
 
 func (r *Resource) PublicCanRead() bool {
 	return (r.Flags & FlagPublicRead) == FlagPublicRead
 }
 
-// CheckRead checks if a given key (may be nil) has access to read from this resource.
-func (r *Resource) CheckRead(key *Key) AccessStatus {
-	if r.PublicCanRead() {
-		return AccessAllowed
+// CheckAccess checks if a given key (may be nil) has access to perform a particular operation on this resource.
+func (r *Resource) CheckAccess(key *Key, op types.OperationType) AccessStatus {
+	if key == nil {
+		// allow public reads
+		if op == types.OperationRead && r.PublicCanRead() {
+			return AccessAllowed
+		}
+		return AccessDenied
 	}
-	return checkRoleAccess(key, r.ReadNodes)
+
+	return r.checkKeyAccess(key, op)
 }
 
-// CheckWrite checks if a given key has access to write to this resource.
-func (r *Resource) CheckWrite(key *Key) AccessStatus {
-	return checkRoleAccess(key, r.WriteNodes)
-}
-
-// CheckModify checks if a given key has access to modify this resource.
-func (r *Resource) CheckModify(key *Key) AccessStatus {
-	return checkRoleAccess(key, r.ModifyNodes)
-}
-
-// CheckDelete checks if a given key has access to delete this resource.
-func (r *Resource) CheckDelete(key *Key) AccessStatus {
-	return checkRoleAccess(key, r.DeleteNodes)
-}
-
-// checkRoleAccess returns the access status for a particular role in an access map.
-func checkRoleAccess(key *Key, accessMap map[string]bool) AccessStatus {
+// checkKeyAccess returns the access status for a particular role in an access map.
+func (r *Resource) checkKeyAccess(key *Key, op types.OperationType) AccessStatus {
+	roa := ResourceOperationAccess{"*", op}
 	// if catch-all role is present, allow or deny based on its status.
-	if status, ok := accessMap["*"]; ok {
+	if status, ok := r.OperationNodes[roa]; ok {
 		if status {
 			return AccessAllowed
 		}
 		return AccessDenied
 	}
+
+	// if the key itself is present, allow or deny based on its status.
+	roa.ID = key.ID
+	if status, ok := r.OperationNodes[roa]; ok {
+		if status {
+			return AccessAllowed
+		}
+		return AccessDenied
+	}
+
 	// check sorted key roles, returning allow or deny based on the first present in the access map.
 	for _, role := range key.Roles {
-		if status, ok := accessMap[role]; ok {
+		roa.ID = role
+		if status, ok := r.OperationNodes[roa]; ok {
 			if status {
 				return AccessAllowed
 			}
 			return AccessDenied
 		}
 	}
+
 	// no access specifiers on this level
 	return AccessNeutral
 }
