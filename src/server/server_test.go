57c2e232be1c32363eededea8060c4551d24a52c
jd <johnnytriple33@gmail.com>
2022-08-28 05:11:11 -0700

    Added more rigorous tests including multiple types of request being issues asynchronously

--- src/server/server_test.go
+++ src/server/server_test.go
@@ -1,13 +1,14 @@
 package server
 
 import (
+	"encoding/base64"
 	"fmt"
 	"fsrv/src/config"
 	"fsrv/src/database/dbimpl/cache"
 	"fsrv/src/database/dbutil"
 	"fsrv/utils/keygen"
-	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"strconv"
 	"testing"
@@ -45,7 +46,7 @@ func makeKeys(t *testing.T, numKeys, keySize, checksumBytes int, salt []byte) (k
 	for i := 0; i < numKeys; i++ {
 		kStr := keygen.GetRand(keySize)
 		key := keygen.MintKey(kStr, salt, checksumBytes)
-		t.Log(key)
+		//t.Log(key)
 		keys = append(keys, key)
 	}
 	return
@@ -55,25 +56,79 @@ func TestServer(t *testing.T) {
 	setup()
 	t.Log(">STARTING SERVER")
 	go run() //run server
-	time.Sleep(500 * time.Millisecond)
+	time.Sleep(10 * time.Millisecond)
 	t.Log(">GENERATING KEYS")
 	randSize, checkSize := cfg.Server.KeyRandomBytes, cfg.Server.KeyCheckBytes
-	keys := makeKeys(t, 20, randSize, checkSize, []byte(cfg.Server.KeyValidationSecret))
+	keys := makeKeys(t, 200000, randSize, checkSize, []byte(cfg.Server.KeyValidationSecret))
 
 	t.Log(">MAKING REQUESTS")
-	for _, key := range keys {
-		url := fmt.Sprintf("http://127.0.0.1:1337/?key=%s", key)
-		req, _ := http.NewRequest("GET", url, nil)
-		res, _ := http.DefaultClient.Do(req)
-		body, _ := io.ReadAll(res.Body)
-		fmt.Println(res)
-		fmt.Println(string(body))
-		err := res.Body.Close()
-		if err != nil {
-			t.Log(err)
-			t.Fail()
+	go func() {
+		for _, key := range keys {
+			url := fmt.Sprintf("http://127.0.0.1:1337/?key=%s", key)
+			req, _ := http.NewRequest("GET", url, nil)
+			res, _ := http.DefaultClient.Do(req)
+			//body, _ := io.ReadAll(res.Body)
+			//fmt.Println(res)
+			//fmt.Println(string(body))
+			err := res.Body.Close()
+			if err != nil {
+				t.Log(err)
+				t.Fail()
+			}
 		}
-	}
+	}()
 
-	time.Sleep(500 * time.Millisecond)
+	go func() {
+		for _, key := range keys {
+			for i := 0; i < rand.Intn(8); i++ {
+				url := fmt.Sprintf("http://127.0.0.1:1337/?key=%s", key)
+				req, _ := http.NewRequest("GET", url, nil)
+				res, _ := http.DefaultClient.Do(req)
+				//body, _ := io.ReadAll(res.Body)
+				//fmt.Println(res)
+				//fmt.Println(string(body))
+				err := res.Body.Close()
+				if err != nil {
+					t.Log(err)
+					t.Fail()
+				}
+			}
+		}
+	}()
+
+	go func() {
+		size, _ := keygen.GetKeySize(randSize, checkSize)
+		for i := 0; i < 1000; i++ {
+			d := keygen.GetRand(size)
+			key := base64.RawURLEncoding.EncodeToString(d)
+			url := fmt.Sprintf("http://127.0.0.1:1337/bad/?key=%s", key)
+			req, _ := http.NewRequest("GET", url, nil)
+			res, _ := http.DefaultClient.Do(req)
+			//body, _ := io.ReadAll(res.Body)
+			//fmt.Println(res)
+			//fmt.Println(string(body))
+			err := res.Body.Close()
+			if err != nil {
+				t.Log(err)
+				t.Fail()
+			}
+		}
+	}()
+
+	func() {
+		for i := 0; i < 2000; i++ {
+			url := fmt.Sprintf("http://127.0.0.1:1337/cool/path/%d", i)
+			req, _ := http.NewRequest("GET", url, nil)
+			res, _ := http.DefaultClient.Do(req)
+			//body, _ := io.ReadAll(res.Body)
+			//fmt.Println(res)
+			//fmt.Println(string(body))
+			err := res.Body.Close()
+			if err != nil {
+				t.Log(err)
+				t.Fail()
+			}
+		}
+	}()
+	time.Sleep(5 * time.Second)
 }
