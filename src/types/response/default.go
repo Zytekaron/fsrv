7a0f232c5fb07f6dcd07ca932847eddfba1f93f7
Michael Thornes <zytekaron@gmail.com>
2022-09-28 21:31:50 -0700

    add and update default responses

--- src/types/response/default.go
+++ src/types/response/default.go
@@ -4,8 +4,9 @@ var EmptySuccess = NewResponse[any](true, "", nil)
 var EmptyError = NewResponse[any](false, "", nil)
 
 var Forbidden = NewErrorMessage("forbidden")
+var ForbiddenExpiredKey = NewErrorMessage("forbidden: expired key")
 var Unauthorized = NewErrorMessage("unauthorized")
-var UnauthorizedExpired = NewErrorMessage("unauthorized: expired key")
 var TooManyRequests = NewErrorMessage("too many requests")
+var TooManyConcurrentRequests = NewErrorMessage("too many concurrent requests")
 
 var InternalServerError = NewErrorMessage("internal server error")
